12435dd4627d4aad6e9b07d6ed000c28200d3bf0
Chris Yu <icethrush@gmail.com>
2019-12-02 17:38:16 -0500

    better mp4 scripts

---
+++ mp4_better.sh
@@ -0,0 +1,1 @@
+ffmpeg -i build/frames/frame%04d.png -c:v libx264 -crf 18 -profile:v main -pix_fmt yuv420p -c:a aac -ac 2 -b:a 128k -movflags faststart output.mp4
--- overlay_transparent.sh
+++ overlay_transparent.sh
@@ -1,1 +1,1 @@
-ffmpeg -n -i $1 -i $2 -filter_complex "[1:v]format=rgba,colorchannelmixer=aa=0.5[fg];[0][fg]overlay" -movflags +faststart overlay.mp4
+ffmpeg -n -i $1 -i $2 -filter_complex "[1:v]format=rgba,colorchannelmixer=aa=0.5[fg];[0][fg]overlay" -c:v libx264 -crf 18 -profile:v main -pix_fmt yuv420p -c:a aac -ac 2 -b:a 128k -movflags faststart overlay.mp4
