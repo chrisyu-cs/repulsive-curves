231afa8449a192aeab87bca0e2910f22a31df3b3
Chris Yu <icethrush@gmail.com>
2019-12-05 11:19:49 -0500

    fix includes

--- include/implicit_surface.h
+++ include/implicit_surface.h
@@ -1,14 +1,16 @@
 #pragma once
 
-#include "poly_curve_network.h"
+#include "geometrycentral/utilities/vector3.h"
 
 namespace LWS {
+
+    using namespace geometrycentral;
 
     class ImplicitSurface {
         public:
         virtual ~ImplicitSurface();
-        virtual double SignedDistance(Vector3 point);
-        virtual Vector3 GradientOfDistance(Vector3 point);
+        virtual double SignedDistance(Vector3 point) = 0;
+        virtual Vector3 GradientOfDistance(Vector3 point) = 0;
     };
 
     class ImplicitSphere : public ImplicitSurface {
